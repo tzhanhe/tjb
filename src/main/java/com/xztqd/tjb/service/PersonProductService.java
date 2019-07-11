ffebd84dc0cf42896c4dcaa16d45a5207c99424d
姚哲韬 <yaozhetao@yaozhetaodeMacBook-Pro.local>
2019-07-11 23:25:13 +0800

    fix:提供返回个人产品接口。

---
+++ src/main/java/com/xztqd/tjb/service/PersonProductService.java
@@ -0,0 +1,18 @@
+package com.xztqd.tjb.service;
+
+import com.xztqd.tjb.bean.PersonProduct;
+import com.xztqd.tjb.dao.PersonProductDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonProductService {
+    @Autowired
+    PersonProductDaoImpl accountDaoImpl;
+
+    public List<PersonProduct> getPersonProduct(){
+        return accountDaoImpl.select();
+    }
+}
