d88e015f5e9ea0f6bb30feaa680464d9f840b7a8
alan-w-255 <1649022341@qq.com>
2019-07-11 08:27:27 +0800

    init commit
    初始化了一个 java web 项目，使用 idea 创建。

---
+++ src/main/java/com/xztqd/tjb/TjbApplication.java
@@ -0,0 +1,13 @@
+package com.xztqd.tjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjbApplication.class, args);
+    }
+
+}
