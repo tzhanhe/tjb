edb6e18655ece7cfd84409029a56b54fc4061aa2
alan-w-255 <1649022341@qq.com>
2019-07-12 10:14:27 +0800

    fix: del unused package, import missing package

--- src/main/java/com/xztqd/tjb/service/RecomProductService.java
+++ src/main/java/com/xztqd/tjb/service/RecomProductService.java
@@ -3,7 +3,6 @@ package com.xztqd.tjb.service;
 import com.xztqd.tjb.bean.RecomProduct;
 import com.xztqd.tjb.dao.RecomProductDaoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
